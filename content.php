c2ed76ab18b6fb8b340254c025961dac008afc07
Chris Wilcoxson <chrisw@dccmarketing.com>
2015-01-30 09:37:03 -0600

    Theme now responsive. Many changes and somehow the git repo didn't get updated.

---
+++ content.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @package CL-Meadowbrook
+ */
+
+?><article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header justcontent"><?php
+
+		the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' );
+
+	?></header><!-- .entry-header -->
+
+	<div class="entry-content"><?php
+
+		the_excerpt();
+
+	?></div><!-- .entry-content --><?php
+
+	if ( 'post' == get_post_type() ) :
+
+		?><div class="entry-meta"><?php
+
+			cl_meadowbrook_posted_on();
+
+		?></div><!-- .entry-meta --><?php
+
+	endif;
+
+	?><footer class="entry-footer"><?php
+
+		cl_meadowbrook_entry_footer();
+
+	?></footer><!-- .entry-footer -->
+</article><!-- #post-## -->
